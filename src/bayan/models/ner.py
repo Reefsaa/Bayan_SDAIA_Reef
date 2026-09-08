205d37e2e607bc3968c149e174eb3f58000dc89f
Wesal <444204758@student.ksu.edu.sa>
2026-09-08 18:50:29 +0300

    Update ner.py

--- src/bayan/models/ner.py
+++ src/bayan/models/ner.py
@@ -1,23 +1,20 @@
-"""Lab 3 starter: NER label alignment."""
+"""Lab 3B: NER label alignment."""
 
 
 def align_labels(word_ids, word_labels):
-    aligned_labels = []
+    aligned = []
     previous_word_id = None
 
     for word_id in word_ids:
         if word_id is None:
-            # Special tokens such as [CLS] and [SEP]
-            aligned_labels.append(-100)
+            aligned.append(-100)
 
         elif word_id != previous_word_id:
-            # First subword gets the original BIO label
-            aligned_labels.append(word_labels[word_id])
+            aligned.append(word_labels[word_id])
 
         else:
-            # Remaining subword pieces are ignored in the loss
-            aligned_labels.append(-100)
+            aligned.append(-100)
 
         previous_word_id = word_id
 
-    return aligned_labels
+    return aligned
